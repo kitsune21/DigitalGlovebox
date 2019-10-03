22c29e065271b55f32f49a70195f055a3f69a1d7
Scott Young <youngscottj@gmail.com>
2019-10-02 19:34:23 -0600

    build documents type controller

---
+++ app/controllers/api/document_types_controller.rb
@@ -0,0 +1,33 @@
+class Api::DocumentTypesController < ApplicationController
+  def index
+    render json: DocumentType.all
+  end
+  def create
+    @documentType = DocumentType.new(documentType_params)
+    if @documentType.save
+      render json: @documentType
+    else
+      render json: { errors: @documentType.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @documentType = DocumentType.find(params[:id])
+    if @documentType.update(documentType_params)
+      render json: @documentType
+    else
+      render json: { errors: @documentType.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    DocumentType.find(params[:id]).destroy
+    render json: { message: 'Document type deleted'}
+  end
+
+  private
+    def documentType_params
+      # { documentType: {title: '', body: ''} }
+    params.require(:documentType).permit(:title, :body)
+  end
+end
