5a0191cefb1a8bd91a84c2cb97967ce0dbe7f9d3
Jennifer Porter <jlporter@myhiddencove.com>
2019-10-02 20:22:14 -0600

    fixed @car

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -4,20 +4,20 @@ def index
 end
 
 def create
-  car = Car.new(car_params)
-  if car.save
+  @car = Car.new(car_params)
+  if @car.save
     render json: car
   else
-    render json: { errors: car.errors }, status: :unprocessable_entity
+    render json: { errors: @car.errors }, status: :unprocessable_entity
   end
 end
 
 def update
-  car = Car.find(params[:id])
-  if car.update(car_params)
-    render json: car
+  @car = Car.find(params[:id])
+  if @car.update(car_params)
+    render json: @car
   else
-    render json { errors: car.errors }, status: :unprocessable_entity
+    render json { errors: @car.errors }, status: :unprocessable_entity
   end
 end
 
@@ -26,7 +26,7 @@ def destroy
   render json: { message: 'Car deleted' }
 end
 
-  private
+private
 
   def car_params
     params.require(:car).permit(:year, :make, :model, :mileage, :vin, :color, :license_plate, :user_id)
