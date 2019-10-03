88a7a327a3a69d35970973cd72b3b4f86bd8f6b1
Jennifer Porter <jlporter@myhiddencove.com>
2019-10-02 19:35:38 -0600

    cars controller

---
+++ app/controllers/api/cars_controller.rb
@@ -0,0 +1,34 @@
+class Api::CarsController < ApplicationController
+def index
+  render json: Car.all 
+end
+
+def create
+  car = Car.new(car_params)
+  if car.save
+    render json: car
+  else
+    render json: { errors: car.errors }, status: :unprocessable_entity
+  end
+end
+
+def update
+  car = Car.find(params[:id])
+  if car.update(car_params)
+    render json: car
+  else
+    render json { errors: car.errors }, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  Car.find(params[:id]).destroy
+  render json: { message: 'Car deleted' }
+end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:year, :make, :model, :mileage, :vin, :color, :license_plate, :user_id)
+  end
+end
