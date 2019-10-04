9a240406061248139ddf5630f2f693a9af769a22
first last <charaexo@icloud.com>
2019-10-03 20:22:15 -0600

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+name = ['Vehicle']
+
+30.times do
+  Vehicle = Vehicle.create(
+    year: Faker::Vehicle.year
+    make: Faker::Vehicle.make
+    model: Faker::Vehicle.model
+    mileage: Faker::Vehicle.mileage
+    vin: Faker::Vehicle.vin
+    color: Faker::Vehicle.color
+    license_plate: Faker::vehicle.license_plate
+  )
+end
+
+30.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    password: 'password',
+  )
+end
+
+user = User.create(
+  email: test.test@tester.com,
+  first_name: Faker::Name.first_name,
+  password: 'password',
+)
+end
+
+puts 'Data Seeded'
+
+
