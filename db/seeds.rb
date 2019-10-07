8dbe9c84312ce4402ef770123867bcd871536b2b
Scott Young <youngscottj@gmail.com>
2019-10-07 00:12:14 -0600

    add faker data for documents

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,24 +4,24 @@ name = %w[Insurance Manual License other]
 30.times do
   user = User.create(
     email: Faker::Internet.email,
-    first_name: Faker::Name.first_name,
+    name: Faker::Name.first_name,
     password: 'password',
   )
-  Vehicle = Vehicle.create(
-    year: Faker::Vehicle.year
-    make: Faker::Vehicle.make
-    model: Faker::Vehicle.model
-    mileage: Faker::Vehicle.mileage
-    vin: Faker::Vehicle.vin
-    color: Faker::Vehicle.color
-    license_plate: Faker::vehicle.license_plate
+  vehicle = Car.create(
+    year: Faker::Vehicle.year,
+    make: Faker::Vehicle.make,
+    model: Faker::Vehicle.model,
+    mileage: Faker::Vehicle.mileage,
+    vin: Faker::Vehicle.vin,
+    color: Faker::Vehicle.color,
+    license_plate: Faker::Vehicle.license_plate,
     user_id: user.id
   )
   document_type = DocumentType.create(
     name: name.sample,
     user_id: user.id
   )
-  Document.create(
+  document = Document.create(
     name: Faker::Superhero.name,
     user_id: user.id,
     document_type_id: document_type.id
