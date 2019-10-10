35cdbbf65717d8881c32bb6e43ca5d4c12353942
Cecil Thomas <kitsunesama21@gmail.com>
2019-10-09 18:43:48 -0600

    re-fixed the carlist cause am bad

--- client/src/Components/Cars/CarList.js
+++ client/src/Components/Cars/CarList.js
@@ -6,10 +6,9 @@ import { AuthConsumer } from '../../Providers/AuthProvider';
  class CarList extends Component {
 
   render() {
-    const { cars } = this.props;
       return (
           <ol>
-           { cars.map( c =>
+           { this.props.cars.map( c =>
                <li key={c.id}>{c.make} {c.model} {c.year}</li>
              )
            }
--- client/src/Components/Dashboard/dashboard.js
+++ client/src/Components/Dashboard/dashboard.js
@@ -7,9 +7,20 @@ import CarList from '../Cars/CarList';
 class Dashboard extends Component {
   state = { cars: []};
 
+  componentDidMount() {
+    const { auth: { user } } = this.props;
+    axios.get(`/api/users/${user.id}/cars`)
+      .then( res => {
+        this.setState({cars: res.data})
+      })
+      .catch( res => {
+        console.log(res)
+      })
+  }
+
   render() {
     return (
-      <CarList cars={this.state.cars}/>
+      <CarList cars={this.state.cars} />
        );
      }
   }
