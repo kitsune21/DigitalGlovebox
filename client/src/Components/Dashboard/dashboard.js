0221d58bc58b26068c3ee6b96b4d6c6a0ede4e9f
Cecil Thomas <kitsunesama21@gmail.com>
2019-10-10 18:13:36 -0600

    started to add custom doc types

--- client/src/Components/Dashboard/dashboard.js
+++ client/src/Components/Dashboard/dashboard.js
@@ -9,13 +9,15 @@ class Dashboard extends Component {
 
   componentDidMount() {
     const { auth: { user } } = this.props;
-    axios.get(`/api/users/${user.id}/cars`)
+    if(user) {
+      axios.get(`/api/users/${user.id}/cars`)
       .then( res => {
         this.setState({cars: res.data})
       })
       .catch( res => {
         console.log(res)
       })
+    }
   }
 
   addItem = (name) => {
