25191f7ff699dcb811c67d3ba6204cf74f404f9b
Cecil Thomas <kitsunesama21@gmail.com>
2019-10-16 07:55:33 -0600

    Added the ability to flip through pages

--- client/src/Components/Documents/DocumentPages/DocumentPages.js
+++ client/src/Components/Documents/DocumentPages/DocumentPages.js
@@ -4,7 +4,7 @@ import DocumentPageItem from './DocumentPageItem';
 
 class DocumentPages extends Component {
 
-  state = {pages: []}
+  state = {pages: [], currentPage: 0}
 
   componentDidMount() {
     axios.get(`/api/documents/${this.props.doc_id}/document_pages`)
@@ -13,13 +13,34 @@ class DocumentPages extends Component {
     })
   }
 
+  renderPage = () => {
+    const {pages, currentPage } = this.state;
+    return(
+      pages.length > 0 ? <DocumentPageItem page={pages[currentPage]}/> : null
+    )
+  }
+
+  nextPage = () => {
+    const {pages, currentPage } = this.state;
+    if(currentPage < pages.length -1) {
+      this.setState({currentPage: this.state.currentPage + 1})
+    }
+  }
+
+  previousPage = () => {
+    const {pages, currentPage } = this.state;
+    if(currentPage > 1) {
+      this.setState({currentPage: this.state.currentPage - 1})
+    }
+  }
+
   render() {
     return(
       <div>
         <h4>Pages</h4>
-        {
-          this.state.pages.map(page => <DocumentPageItem page={page}/> )
-        }
+        {this.renderPage()}
+        <button onClick={() => this.previousPage()}>Previous Page</button>
+        <button onClick={() => this.nextPage()}>Next Page</button>
       </div>
     )
   }
