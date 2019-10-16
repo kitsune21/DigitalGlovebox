303ef96c7dc6428120c7113c68070a5457255253
Scott Young <youngscottj@gmail.com>
2019-10-15 20:27:58 -0600

    all crud actions working

--- client/src/Components/Documents/UserDocumentTypes.js
+++ client/src/Components/Documents/UserDocumentTypes.js
@@ -63,9 +63,18 @@ class UserDocumentTypes extends Component {
   }
 
   renderDocTypes = () => {
+    const { editing } = this.state
     return(
       this.state.document_types.map( type =>
         <div key={type.id} >
+        {
+          editing ?
+          <DocTypeForm
+          update={this.updateDocType}
+          toggleEdit={this.toggleEdit}
+          id={type.id}
+          /> :
+          <>
           <h2>{type.name}</h2>
           <Button onClick={this.toggleEdit}>
             Edit
@@ -73,6 +82,8 @@ class UserDocumentTypes extends Component {
           <Button onClick={ () => this.deleteDocType(type.id) }>
             Delete
           </Button>
+          </>
+        }
           {
             this.renderDocs(type.id).map( doc =>
             <DocumentItem key={doc.id} myDocument={doc} />
