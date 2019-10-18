dfd1d88d5eec1ecd0bbc3355e4c4d996349a79d1
first last <charaexo@icloud.com>
2019-10-17 20:40:17 -0600

    styled

--- client/src/components/shared/Navbar.js
+++ client/src/components/shared/Navbar.js
@@ -1,31 +1,82 @@
 import React from 'react';
 import { Link, withRouter, } from 'react-router-dom';
 import { AuthConsumer } from '../../Providers/AuthProvider';
+import { Menu } from 'semantic-ui-react';
+import styled from 'styled-components';
 
-class Navbar extends React.Component{
+class Navbar extends React.Component {
+
+  rightNavItems = () => {
+    const { auth: { user, handleLogout, }, location, } = this.props;
+
+    if (user) {
+      return (
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='logout'
+            onClick={ () => handleLogout(this.props.history) }
+          />
+        </Menu.Menu>
+      )
+    } else {
+      return (
+        <Menu.Menu position='right'>
+          <Link to='/login'>
+            <Menu.Item
+              id='login'
+              name='login'
+              active={location.pathname === '/login'}
+            />
+          </Link>
+          <Link to='/register'>
+            <Menu.Item
+              id='register'
+              name='register'
+              active={location.pathname === '/register'}
+            />
+          </Link>
+        </Menu.Menu>
+      )
+    }
+  }
+
   render() {
-    const { auth: { user, handleLogout, }, } = this.props;
     return (
       <div>
-        <ul>
-          <li><Link to='/'>Dashboard</Link></li>
-          <li><Link to='/documents'>Documents</Link></li>
-          <li>Contact Us</li>
-          {
-            user ? <li><Link to='/profile'>{user.name}</Link><button onClick={ () => handleLogout(this.props.history) }>Logout</button></li> : <li><Link to='/login' >Login</Link></li>
-          }
-          
-        </ul>
+        <Menu pointing secondary inverted>
+          <Link to='/'>
+            <Menu.Item
+              name='dashboard'
+              id='dashboard'
+              active={this.props.location.pathname === '/'}
+            />
+          </Link>
+          <Link to='/documents'>
+            <Menu.Item
+              name='documents'
+              id='documents'
+              active={this.props.location.pathname === '/documents'}
+            />
+          </Link>
+          <Link to='#'>
+            <Menu.Item
+              name='contact us'
+              id='contact us'
+              active={this.props.location.pathname === '/#'}
+            />
+          </Link>
+            { this.rightNavItems() }
+        </Menu>
       </div>
-    );
+    )
   }
 }
 
 export class ConnectedNavbar extends React.Component {
   render() {
     return (
-      <AuthConsumer> 
-        { auth => 
+      <AuthConsumer>
+        { auth =>
           <Navbar { ...this.props } auth={auth} />
         }
       </AuthConsumer>
