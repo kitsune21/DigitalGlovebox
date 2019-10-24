cf84dba4caf2ede6f5fb7e5d4ed4828317f216cc
Scott Young <youngscottj@gmail.com>
2019-10-23 19:18:32 -0600

    what to do after an accident

---
+++ client/src/Components/Shared/AfterAnAccident.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react'
+
+const AfterAnAccident = ({}) => (
+  <Container style={
+    {
+      backgroundColor: 'white',
+      height: 'auto',
+    }
+  }>
+    <img src="/emergency_list.png" alt="emergency list"/>
+  </Container>
+)
+
+export default AfterAnAccident;
